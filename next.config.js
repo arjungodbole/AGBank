5b246d3c80d63c0fc142f2877ee0197a82476d0d
arjungodbole <167500401+arjungodbole@users.noreply.github.com>
2025-08-06 11:10:12 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true, // ✅ Ignore TypeScript errors during build
+    },
+    eslint: {
+      ignoreDuringBuilds: true, // ✅ Ignore ESLint errors during build
+    },
+  };
+  
+  module.exports = nextConfig;
